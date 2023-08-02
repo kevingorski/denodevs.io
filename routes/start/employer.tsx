71b70b780804faad0e21e160287d4496fdbe8a56
Kevin Gorski <kevin.gorski@gmail.com>
2023-08-02 12:10:45 -0600

    Start of employer flow

--- routes/start/employer.tsx
+++ routes/start/employer.tsx
@@ -15,6 +15,26 @@ export default function EmployerPage() {
         Hello employer!
       </h1>
 
+      <p>
+        Welcome to DenoDevs! Here you can sign up to be matched with developers
+        for your open position or contract.
+      </p>
+
+      <p>
+        DenoDevs is still in its early stages, which presents some unique
+        challenges and opportunities for employers like yourself:
+      </p>
+
+      <ul>
+        <li>There are few developers on the platform (for now)</li>
+        <li>There will be some rough edges as the platform matures</li>
+        <li>
+          Your input is early and more likely to direct DenoDevs toward your
+          needs
+        </li>
+        <li>There's no cost to you to sign up</li>
+      </ul>
+
       <h2>
         <a href="/">Home</a>
       </h2>
