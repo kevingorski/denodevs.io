be37e4682e0fe01578d0d9e0936ec7799feee64e
Kevin Gorski <kevin.gorski@gmail.com>
2023-08-10 16:42:54 -0600

    Add more text to employer LIH

--- routes/employer/index.tsx
+++ routes/employer/index.tsx
@@ -12,6 +12,20 @@ export default function EmployerHomePage(props: PageProps<EmployerState>) {
     <main>
       <h1>Welcome {props.data.employer.name}!</h1>
       <h2>Representing {props.data.employer.company}</h2>
+
+      <p>
+        Thank you for joining Deno Devs as an employer! There isn't anything
+        else for you to do for the moment, but we'll use the email address you
+        provided ({props.data.employer.email}) to let you know the next steps in
+        finding your next Deno developer.
+      </p>
+
+      <p>
+        In the meantime, if you have any questions about Deno Devs, you can{" "}
+        <a href="mailto:kevin@denodevs.io" title="Email Kevin re: Deno Devs">
+          email the me (founder of Deno Devs) directly here
+        </a>.
+      </p>
     </main>
   );
 }
