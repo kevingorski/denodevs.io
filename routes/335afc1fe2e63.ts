528a7e761d9209a1c24bf50bbb204c7a923fd6cc
Kevin Gorski <kevin.gorski@gmail.com>
2023-09-13 17:54:34 -0600

    Also proxy POST

--- routes/335afc1fe2e63.ts
+++ routes/335afc1fe2e63.ts
@@ -1,20 +1,24 @@
 /* Reverse proxy for Clicky Beacon to enable analytics */
 import { HandlerContext, Handlers } from "$fresh/server.ts";
 
+async function proxyClickyRequest(req: Request, ctx: HandlerContext) {
+  const { host, protocol, search } = new URL(req.url);
+  const proxiedUrl = new URL("https://in.getclicky.com/in.php");
+  proxiedUrl.search = search;
+
+  const headers = new Headers();
+  headers.set("Host", proxiedUrl.hostname);
+  headers.set("X-Forwarded-Host", host);
+  headers.set("X-Forwarded-Proto", protocol);
+  headers.set("X-Forwarded-For", ctx.remoteAddr.hostname);
+
+  return await fetch(proxiedUrl, {
+    headers,
+    method: req.method,
+  });
+}
+
 export const handler: Handlers = {
-  async GET(req: Request, ctx: HandlerContext) {
-    const { host, protocol, search } = new URL(req.url);
-    const proxiedUrl = new URL("https://in.getclicky.com/in.php");
-    proxiedUrl.search = search;
-
-    const headers = new Headers();
-    headers.set("Host", proxiedUrl.hostname);
-    headers.set("X-Forwarded-Host", host);
-    headers.set("X-Forwarded-Proto", protocol);
-    headers.set("X-Forwarded-For", ctx.remoteAddr.hostname);
-
-    return await fetch(proxiedUrl, {
-      headers,
-    });
-  },
+  GET: proxyClickyRequest,
+  POST: proxyClickyRequest,
 };
