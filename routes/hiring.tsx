b83542af35e54dfee17335fda8b5102167531b60
Kevin Gorski <kevin.gorski@gmail.com>
2023-11-01 18:54:25 -0600

    Updated hiring page

--- routes/hiring.tsx
+++ routes/hiring.tsx
@@ -22,15 +22,9 @@ export default function HiringPage() {
   return (
     <main>
       <h1>
-        Get matched with developers looking for{"  "}
+        Get matched with developers looking for{" "}
         <a href="https://deno.com">Deno</a> jobs
       </h1>
-
-      <p>
-        Welcome to{" "}
-        {SITE_NAME}! Here you can sign up to be matched with developers for your
-        open jobs.
-      </p>
 
       <p>
         Stop wasting time posting to job boards hoping to find devs looking to
@@ -47,13 +41,43 @@ export default function HiringPage() {
       </p>
 
       <ul>
-        <li>There are few developers on the platform (for now)</li>
-        <li>There will be some rough edges as the platform matures</li>
+        <li>There are few developers (for now)</li>
+        <li>There will be some rough edges</li>
         <li>
           Your input is early and more likely to direct {SITE_NAME}{" "}
           toward your needs
         </li>
-        <li>There's no cost to you to sign up</li>
+      </ul>
+
+      <p>
+        Once there are enough developers on{" "}
+        {SITE_NAME}, the matching process is expected to look like this:
+      </p>
+
+      <ul>
+        <li>
+          {SITE_NAME}{" "}
+          will send an email letting you know we're ready for job listings
+        </li>
+        <li>
+          When you're looking to hire for a job, you'll share details about it
+          via the site or email
+        </li>
+        <li>
+          {SITE_NAME}{" "}
+          will send you anonymized, matching developer profiles on a weekly
+          basis as long as the position is open and you have an active
+          subscription
+        </li>
+        <li>
+          Any developers interested in the position will respond to you and
+          their full profiles will be available to you
+        </li>
+        <li>You decide whether to continue and hire one of the deveopers</li>
+        <li>
+          If a hire is made, {SITE_NAME}{" "}
+          earns a 10% of first-year salary or contract value success award
+        </li>
       </ul>
 
       {CTA()}
