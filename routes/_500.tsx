808cb85eefa96ee918d31f8849e095697b2afc81
Kevin Gorski <kevin.gorski@gmail.com>
2023-12-20 09:41:36 -0700

    500 with a better message and support link

--- routes/_500.tsx
+++ routes/_500.tsx
@@ -1,10 +1,27 @@
-import { ErrorPageProps } from "$fresh/server.ts";
+import { PageProps } from "$fresh/server.ts";
+import ContactSupportLink from "@/components/ContactSupportLink.tsx";
+import { SITE_NAME } from "@/utils/constants.ts";
 
-export default function Error500Page(props: ErrorPageProps) {
+export default function Error500Page({ error, url }: PageProps) {
+  const id = crypto.randomUUID();
+  const supportMessageBody = `Hello, this is [Your Name Here].
+  I was on ${SITE_NAME} at "${url}", but I ran into an error (ref ${id}).
+  Here are more details:
+  [Your Report Here]
+  Please help!`;
+  console.error(error, id);
   return (
     <main>
-      <h1>Server error</h1>
-      <p>500 internal error: {(props.error as Error).message}</p>
+      <h1>Server error ☄️</h1>
+      <p>
+        Something went wrong on our side &mdash; the issue has been logged, but
+        if you'd like to provide more context, please{" "}
+        <ContactSupportLink
+          linkText="contact Kevin"
+          messageBody={supportMessageBody}
+          messageSubject="Deno Devs Error "
+        />.
+      </p>
       <p>
         <a href="/">Return home</a>
       </p>
