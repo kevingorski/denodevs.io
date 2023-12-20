c8d02a5e0c5866922bed090f8923ae86bfe1358e
Kevin Gorski <kevin.gorski@gmail.com>
2023-12-20 08:37:45 -0700

    No thanks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,6 @@ export default function Footer(
           titleText={`Send ${SITE_NAME} Feedback`}
         />
         <a href="https://techhub.social/@denodevs" rel="me">Mastodon</a>
-        <a href="https://twitter.com/denodevs" rel="me">Twitter</a>
         <a
           href="https://github.com/kevingorski/denodevs.io"
           target="_blank"
